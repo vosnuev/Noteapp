6e7c186210b6ca918fa8b665c27c4a7352b711d0
vosnuev <vosnuevo@gmail.com>
2026-09-15 23:06:28 +0900

    chore: single-file HTML build setup with vite-plugin-singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  build: {
+    assetsInlineLimit: 100000000, // 인라인화 한도 크게
+    cssCodeSplit: false,
+  },
 })
